d812a5a757f889e1b771e190250b27d890570a5c
Robert Kempf <rvkempf@gmail.com>
2024-02-15 13:37:07 -0500

    Stub out app (#2)

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,25 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
+import { Form, redirect } from '@remix-run/react';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: 'Green Thumbly' },
+    { name: 'description', content: 'Welcome to Green Thumbly!' },
   ];
+};
+
+export const action = async () => {
+  return redirect('/chooseplants');
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1>Where do you live?</h1>
+      <Form method="post">
+        <input type="text" name="zip" />
+        <button type="submit">Let&apos;s grow!</button>
+      </Form>
     </div>
   );
 }
---
+++ app/routes/chooseplants.tsx
@@ -0,0 +1,30 @@
+import { Form, redirect, useLoaderData } from '@remix-run/react';
+import plantData from '../data/planting_data.json';
+
+// Use mock data for now
+export const loader = () => {
+  return plantData;
+};
+
+export const action = async () => {
+  return redirect('/gardenplanner');
+};
+
+export default function ChoosePlants() {
+  const plantData = useLoaderData<typeof loader>();
+  return (
+    <div>
+      <h1>What are you growing?</h1>
+
+      <Form method="post">
+        {Object.keys(plantData).map((plant) => (
+          <div key={plant}>
+            <input type="checkbox" name={plant} id={plant} />
+            <label htmlFor={plant}>{plant}</label>
+          </div>
+        ))}
+        <button type="submit">Let&apos;s grow!</button>
+      </Form>
+    </div>
+  );
+}
