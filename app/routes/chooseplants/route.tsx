c0a461983d2d56f0464e75d1e29d04a54715074e
Robert Kempf <rvkempf@gmail.com>
2024-02-21 16:35:19 -0500

    Improve the plant selection UI (#4)

--- app/routes/chooseplants/route.tsx
+++ app/routes/chooseplants/route.tsx
@@ -1,6 +1,7 @@
 import { Form, redirect, useLoaderData } from '@remix-run/react';
 import plantData from '~/data/planting_data.json';
 import styles from './styles.module.css';
+import { useState } from 'react';
 
 // Use mock data for now
 export const loader = () => {
@@ -13,17 +14,51 @@ export const action = async () => {
 
 export default function ChoosePlants() {
   const plantData = useLoaderData<typeof loader>();
+  const [selectedPlants, setSelectedPlants] = useState<string[]>([]);
   return (
     <div className={styles.container}>
       <h1>What are you growing?</h1>
 
       <Form method="post">
-        {Object.entries(plantData).map(([key, { name }]) => (
-          <div key={key}>
-            <input type="checkbox" name={name} id={key} />
-            <label htmlFor={name}>{name}</label>
+        <div className={styles.formContainer}>
+          <div>
+            {Object.entries(plantData).map(([key, { name }]) => (
+              <div key={key}>
+                <input
+                  type="checkbox"
+                  id={key}
+                  name={name}
+                  value={name}
+                  checked={selectedPlants.includes(name)}
+                  onChange={(e) => {
+                    if (e.target.checked) {
+                      setSelectedPlants([...selectedPlants, name]);
+                    } else {
+                      setSelectedPlants(
+                        selectedPlants.filter((p) => p !== name),
+                      );
+                    }
+                  }}
+                />
+                <label htmlFor={name}>{name}</label>
+              </div>
+            ))}
           </div>
-        ))}
+
+          <div>
+            <h2>I'm growing:</h2>
+            <ul>
+              {selectedPlants.length ? (
+                selectedPlants
+                  .sort()
+                  .map((plant) => <li key={plant}>{plant}</li>)
+              ) : (
+                <span style={{ fontStyle: 'italic' }}>Select your plants</span>
+              )}
+            </ul>
+          </div>
+        </div>
+
         <button type="submit">Let&apos;s grow!</button>
       </Form>
     </div>
