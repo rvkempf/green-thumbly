038b8c66c141eb4f012b7621429ebae52b474a30
rvkempf <rvkempf@gmail.com>
2024-03-02 19:31:38 -0500

    done

--- app/components/PlanningChart/PlanningChart.tsx
+++ app/components/PlanningChart/PlanningChart.tsx
@@ -3,6 +3,10 @@ import styles from './PlanningChart.module.css';
 import { PlantRow } from './components/PlantRow/PlantRow';
 import { MonthAxis } from './components/MonthAxis/MonthAxis';
 import { WeekAxis } from './components/WeekAxis/WeekAxis';
+import { FrostLine } from './components/LastFrostLine/LastFrostLine';
+
+const lastFrostDay = 80;
+const firstFrostDay = 315;
 
 interface PlanningChartProps {
   plants: PlantCollection;
@@ -12,20 +16,27 @@ export const PlanningChart: React.FC<PlanningChartProps> = ({ plants }) => {
   const rows = plants.map((plant, i) => (
     <PlantRow
       key={plant.name}
-      lastFrostDay={80}
-      firstFrostDay={315}
+      lastFrostDay={lastFrostDay}
+      firstFrostDay={firstFrostDay}
       plant={plant}
       row={i + 3}
     />
   ));
+  const numRows = rows.length + 2;
   return (
     <div
       className={styles.chart}
-      style={{ gridTemplateRows: `repeat(${rows.length + 2}, 1fr)` }}
+      style={{ gridTemplateRows: `repeat(${numRows}, 1fr)` }}
     >
       <MonthAxis />
       <WeekAxis />
       {rows}
+      <FrostLine frostDay={lastFrostDay} firstOrLast="last" numRows={numRows} />
+      <FrostLine
+        frostDay={firstFrostDay}
+        firstOrLast="first"
+        numRows={numRows}
+      />
     </div>
   );
 };
