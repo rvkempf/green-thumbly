57babadc406eaac0c85d8e8c3b74358b58e71b49
rvkempf <rvkempf@gmail.com>
2024-03-08 12:39:09 -0500

    you guys want some cookies?

--- app/routes/gardenplanner/route.tsx
+++ app/routes/gardenplanner/route.tsx
@@ -1,13 +1,25 @@
 import { PlanningChart } from '~/components/PlanningChart/PlanningChart';
 import plantData from '~/data/planting_data.json';
 import styles from './styles.module.css';
+import { LoaderFunctionArgs, json } from '@remix-run/node';
+import { plants } from '~/cookies/plants';
+import { useLoaderData } from '@remix-run/react';
 
-const testPlants = Object.entries(plantData).map(([_, data]) => data);
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const cookieHeader = request.headers.get('Cookie');
+  const cookie: { plants?: string[] } =
+    (await plants.parse(cookieHeader)) || {};
+  return json({ plants: cookie.plants || [] });
+};
 
 export default function GardenPlanner() {
+  const selectedPlants = useLoaderData<typeof loader>().plants;
+  const filteredPlants = Object.entries(plantData)
+    .filter(([_, { name }]) => selectedPlants.includes(name))
+    .map(([_, data]) => data);
   return (
     <div className={styles.chartContainer}>
-      <PlanningChart plants={testPlants} />
+      <PlanningChart plants={filteredPlants} />
     </div>
   );
 }
